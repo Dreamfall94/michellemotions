ced2608e4e6bf13cfe04452e4be2c39cdf3a1064
Dreamfall94 <len4ik2394@gmail.com>
2018-02-16 17:31:51 +0500

    serenity

--- emotions.py
+++ emotions.py
@@ -1,27 +1,42 @@
 class Reaction():
-	pass
+	def __init__(self):
+		self.face = "base.png"
+
+	def __call__(self, reaction):
+		self.face = reaction.face
 
 class Incorporation(Reaction):
-	pass
+	def __init__(self, level = 0):
+		self.face = "0.png"
 
+class Serenity(Incorporation):
+	def __init__(self, level = 1):
+		self.face = "0.png"
+		
 class Rejection(Reaction):
-	pass
+	def __init__(self, level = 0):
+		self.face = "0.png"
 
 class Patronage(Reaction):
-	pass
+	def __init__(self, level = 0):
+		self.face = "0.png"
 
 class Destruction(Reaction):
-	pass
+	def __init__(self, level = 0):
+		self.face = "0.png"
 
 class Reproduction(Reaction):
-	pass
+	def __init__(self, level = 0):
+		self.face = "0.png"
 
 class Reintegration(Reaction):
-	pass
+	def __init__(self, level = 0):
+		self.face = "0.png"
 
 class Orientation(Reaction):
-	pass
+	def __init__(self, level = 0):
+		self.face = "0.png"
 
 class Exploration(Reaction):
-	pass
-
+	def __init__(self, level = 0):
+		self.face = "0.png"
