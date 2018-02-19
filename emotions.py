63de597216e781c0c3296c18a54b75bd0fb0fcea
Dreamfall94 <len4ik2394@gmail.com>
2018-02-19 19:01:07 +0500

    interface+emotion

--- emotions.py
+++ emotions.py
@@ -2,6 +2,7 @@ from tkinter import PhotoImage
 
 class Reaction():
 	def __init__(self):
+		self.text = ""
 		self.id = 'base'
 		self.face = PhotoImage(file =  "img/base.png")
 
@@ -14,40 +15,163 @@ class Reaction():
 class Base(Reaction):
 	pass
 
-class Incorporation(Reaction):
+class Incorporation(Reaction):				#Инкорпорация
 	def __init__(self, level = 0):
+		self.text = "Инкорпорация"
 		self.id = 'Incorporation'
-		self.face = PhotoImage(file =  "img/Incorporation/Incorporation1.png")
+		self.face = PhotoImage(file =  "img/base.png")
 
-class Serenity(Incorporation):
+class Serenity(Incorporation):				#Инкорпорация: безмятежность(нет пнг)
 	def __init__(self, level = 1):
 		self.id = 'Serenity'
-		self.face = PhotoImage(file =  "img/Incorporation/Incorporation2.png")
+		self.face = PhotoImage(file =  "img/base.png")
 
-class Rejection(Reaction):
-	def __init__(self, level = 0):
-		self.face = "0.png"
+class Joy(Incorporation):					#Инкорпорация: радость
+	def __init__(self, level = 2):
+		self.id = 'Joy'
+		self.face = PhotoImage(file =  "img/Incorporation/joy.png")
 
-class Patronage(Reaction):
-	def __init__(self, level = 0):
-		self.face = "0.png"
+class Delight(Incorporation):				#Инкорпорация: восторг
+	def __init__(self, level = 3):
+		self.id = 'Delight'
+		self.face = PhotoImage(file =  "img/Incorporation/delight.png")
 
-class Destruction(Reaction):
+class Rejection(Reaction):					#Отвержение
 	def __init__(self, level = 0):
-		self.face = "0.png"
+		self.id = 'Rejection'
+		self.face = PhotoImage(file =  "img/base.png")
 
-class Reproduction(Reaction):
-	def __init__(self, level = 0):
-		self.face = "0.png"
+class Boredom(Rejection):					#Отвержение: скука
+	def __init__(self, level = 1):
+		self.id = 'Boredom'
+		self.face = PhotoImage(file =  "img/Rejection/boredom.png")
 
-class Reintegration(Reaction):
-	def __init__(self, level = 0):
-		self.face = "0.png"
+class Displeasure(Rejection):				#Отвержение: неудовольствие
+	def __init__(self, level = 2):
+		self.id = 'Displeasure'
+		self.face = PhotoImage(file =  "img/Rejection/displeasure.png")
 
-class Orientation(Reaction):
-	def __init__(self, level = 0):
-		self.face = "0.png"
+class Disgust(Rejection):			 		#Отвержение: отвращение
+	def __init__(self, level = 3):
+		self.id = 'Disgust'
+		self.face = PhotoImage(file =  "img/Rejection/disgust.png")
 
-class Exploration(Reaction):
+class Patronage(Reaction):					#Протекция
 	def __init__(self, level = 0):
-		self.face = "0.png"
+		self.id = 'Patronage'
+		self.face = PhotoImage(file =  "img/base.png")
+
+class Alarm(Patronage):						#Протекция: тревога
+	def __init__(self, level = 1):
+		self.id = 'Alarm'
+		self.face = PhotoImage(file =  "img/Patronage/alarm.png")
+
+class Fear(Patronage):						#Протекция: страх
+	def __init__(self, level = 2):
+		self.id = 'Fear'
+		self.face = PhotoImage(file =  "img/Patronage/fear.png")
+
+class Horror(Patronage):					#Протекция: ужас(нет пнг)
+	def __init__(self, level = 3):
+		self.id = 'Horror'
+		self.face = PhotoImage(file =  "img/base.png")
+
+class Destruction(Reaction):				#Разрушение
+	def __init__(self, level = 0):
+		self.id = 'Destruction'
+		self.face = PhotoImage(file =  "img/base.png")
+
+class Annoyance(Destruction):				#Разрушение: досада
+	def __init__(self, level = 1):
+		self.id = 'Annoyance'
+		self.face = PhotoImage(file =  "img/Destruction/annoyance.png")
+
+class Spite(Destruction):					#Разрушение: злость
+	def __init__(self, level = 2):
+		self.id = 'Spite'
+		self.face = PhotoImage(file =  "img/Destruction/spite.png")
+
+class Anger(Destruction):					#Разрушение: гнев
+	def __init__(self, level = 3):
+		self.id = 'Anger'
+		self.face = PhotoImage(file =  "img/Destruction/anger.png")
+
+class Reproduction(Reaction): 				#Воспроизводство
+	def __init__(self, level = 0):
+		self.id = 'Reproduction'
+		self.face = PhotoImage(file =  "img/base.png")
+
+class Excitation(Reproduction): 			#Воспроизводство: возбуждение
+	def __init__(self, level = 1):
+		self.id = 'Excitation'
+		self.face = PhotoImage(file =  "img/Reproduction/excitation.png")
+
+class Surprise(Reproduction): 				#Воспроизводство: удивление
+	def __init__(self, level = 2):
+		self.id = 'Surprise'
+		self.face = PhotoImage(file =  "img/Reproduction/surprise.png")
+
+class Amazement(Reproduction): 				#Воспроизводство: изумление
+	def __init__(self, level = 3):
+		self.id = 'Amazement'
+		self.face = PhotoImage(file =  "img/Reproduction/amazement.png")
+
+class Reintegration(Reaction):				#Реинтеграция
+	def __init__(self, level = 0):
+		self.id = 'Reintegration'
+		self.face = PhotoImage(file =  "img/base.png")
+
+class Sorrow(Reintegration):				#Реинтеграция: печаль
+	def __init__(self, level = 1):
+		self.id = 'Sorrow'
+		self.face = PhotoImage(file =  "img/Reintegration/sorrow.png")
+
+class Sadness(Reintegration):				#Реинтеграция: грусть
+	def __init__(self, level = 2):
+		self.id = 'Sadness'
+		self.face = PhotoImage(file =  "img/Reintegration/sadness.png")
+
+class Grief(Reintegration):					#Реинтеграция: горе
+	def __init__(self, level = 3):
+		self.id = 'Grief'
+		self.face = PhotoImage(file =  "img/Reintegration/grief.png")
+
+class Orientation(Reaction):				#Ориентация
+	def __init__(self, level = 0):
+		self.id = 'Orientation'
+		self.face = PhotoImage(file =  "img/base.png")
+
+class Interest(Orientation):				#Ориентация: интерес
+	def __init__(self, level = 1):
+		self.id = 'Interest'
+		self.face = PhotoImage(file =  "img/Orientation/interest.png")
+
+class Expectation(Orientation):				#Ориентация: ожидание
+	def __init__(self, level = 2):
+		self.id = 'Expectation'
+		self.face = PhotoImage(file =  "img/Orientation/expectation.png")
+
+class Alertness(Orientation):				#Ориентация: настороженность
+	def __init__(self, level = 3):
+		self.id = 'Alertness'
+		self.face = PhotoImage(file =  "img/Orientation/alertness.png")
+
+class Exploration(Reaction):				#Исследование
+	def __init__(self, level = 0):
+		self.id = 'Exploration'
+		self.face = PhotoImage(file =  "img/base.png")
+
+class Adoption(Exploration):				#Исследование: принятие
+	def __init__(self, level = 1):
+		self.id = 'Adoption'
+		self.face = PhotoImage(file =  "img/Exploration/adoption.png")
+
+class Trust(Exploration):					#Исследование: доверие
+	def __init__(self, level = 2):
+		self.id = 'Trust'
+		self.face = PhotoImage(file =  "img/Exploration/trust.png")
+
+class Admiration(Exploration):				#Исследование: восхищение
+	def __init__(self, level = 3):
+		self.id = 'Admiration'
+		self.face = PhotoImage(file =  "img/Exploration/admiration.png")
--- inter.py
+++ inter.py
@@ -11,16 +11,37 @@ class Face(Tk):
         self.label.configure(image = self.emotion.face)
         self.label.pack()
 
-        self.emotion_to = Incorporation()
-        self.after(500, self.change_emotion)
+        Button(self, text = 'Joy', command = lambda: self.change_emotion(Joy()) ).pack()
+        Button(self, text = 'Horror', command = lambda: self.change_emotion(Horror()) ).pack()
+        Button(self, text = 'Serenity', command = lambda: self.change_emotion(Serenity())).pack()
+        Button(self, text = 'Delight', command = lambda: self.change_emotion(Delight())).pack()
+        Button(self, text = 'Alarm', command = lambda: self.change_emotion(Alarm())).pack()
+        Button(self, text = 'Boredom', command = lambda: self.change_emotion(Boredom())).pack()
+        Button(self, text = 'Displeasure', command = lambda: self.change_emotion(Displeasure())).pack()
+        Button(self, text = 'Disgust', command = lambda: self.change_emotion(Disgust())).pack()
+        Button(self, text = 'Fear', command = lambda: self.change_emotion(Fear())).pack()
+        Button(self, text = 'Annoyance', command = lambda: self.change_emotion(Annoyance())).pack()
+        Button(self, text = 'Spite', command = lambda: self.change_emotion(Spite())).pack()
+        Button(self, text = 'Anger', command = lambda: self.change_emotion(Anger())).pack()
+        Button(self, text = 'Excitation', command = lambda: self.change_emotion(Excitation())).pack()
+        Button(self, text = 'Surprise', command = lambda: self.change_emotion(Surprise())).pack()
+        Button(self, text = 'Amazement', command = lambda: self.change_emotion(Amazement())).pack()
+        Button(self, text = 'Sorrow', command = lambda: self.change_emotion(Sorrow())).pack()
+        Button(self, text = 'Sadness', command = lambda: self.change_emotion(Sadness())).pack()
+        Button(self, text = 'Grief', command = lambda: self.change_emotion(Grief())).pack()
+        Button(self, text = 'Interest', command = lambda: self.change_emotion(Interest())).pack()
+        Button(self, text = 'Expectation', command = lambda: self.change_emotion(Expectation())).pack()
+        Button(self, text = 'Alertness', command = lambda: self.change_emotion(Alertness())).pack()
+        Button(self, text = 'Adoption', command = lambda: self.change_emotion(Adoption())).pack()
+        Button(self, text = 'Trust', command = lambda: self.change_emotion(Trust())).pack()
+        Button(self, text = 'Admiration', command = lambda: self.change_emotion(Admiration())).pack()
 
-    def change_emotion(self):
-        if self.emotion == self.emotion_to:
+    def change_emotion(self, emo):
+        if self.emotion == emo:
             pass
         else :
-            self.label.configure(image = self.emotion_to.face)
-            self.emotion = self.emotion_to
-
+            self.label.configure(image = emo.face)
+            self.emotion = emo
 
 if __name__ == '__main__':
     face = Face()
