79f2c122fb25dfa7c52fd58a5ca61407a43d0fbc
Dreamfall94 <len4ik2394@gmail.com>
2018-02-20 17:23:31 +0500

    person

--- person.py
+++ person.py
@@ -2,48 +2,84 @@ from tkinter import *
 
 
 class Person():                 #Личность
-    pass
+    def __init__(self, sex = 'm', age = 20):
+        self.sex = sex
+        self.age = age
+
+    def __eq__(self, obj):
+        print(self.age, obj.age)
+        return self.sex == obj.sex and self.age == obj.age
+
 
 class Male(Person):             #Мужчина
-    pass
+    def __init__(self):
+        self.sex = 'm'
+
+    def __eq__(self, obj):
+        return self.sex == obj.sex
 
 class Female(Person):           #Женщина
-    pass
+    def __init__(self):
+        self.sex = 'f'
+
+    def __eq__(self, obj):
+        return self.sex == obj.sex
 
 class Child(Person):            #Ребенок
-    pass
+    def __init__(self, age = 7):
+        self.age = age
+
+    def __eq__(self, obj):
+        return obj.age < 15
 
 class Teen(Person):             #Подросток
-    pass
+    def __init__(self, age = 16):
+        self.age = age
+
+    def __eq__(self, obj):
+        return 14 < obj.age < 20
 
 class Adult(Person):            #Взрослый
-    pass
+    def __init__(self, age = 32):
+        self.age = age
+
+    def __eq__(self, obj):
+        return 20 < obj.age < 45
 
 class Aged(Person):             #В возрасте
-    pass
+    def __init__(self, age = 50):
+        self.age = age
 
-class Boy(Male, Child):         #Мальчик
-    pass
+    def __eq__(self, obj):
+        return 45 < obj.age
 
-class Guy(Male, Teen):          #Парень
-    pass
-
-class Man(Male, Adult):         #Мужчина
-    pass
-
-class OldMan(Male, Aged):       #Мужчина в возрасте
-    pass
-
-class Girlie(Female, Child):    #Девочка
-    pass
-
-class Girl(Female, Teen):       #Девушка
-    pass
-
-class Woman(Female, Adult):     #Женщина
-    pass
-
-class OldWoman(Female, Aged):   #Женщина в возрасте
-    pass
+# class Boy(Male, Child):         #Мальчик
+#     pass
+#
+# class Guy(Male, Teen):          #Парень
+#     pass
+#
+# class Man(Male, Adult):         #Мужчина
+#     pass
+#
+# class OldMan(Male, Aged):       #Мужчина в возрасте
+#     pass
+#
+# class Girlie(Female, Child):    #Девочка
+#     pass
+#
+# class Girl(Female, Teen):       #Девушка
+#     pass
+#
+# class Woman(Female, Adult):     #Женщина
+#     pass
+#
+# class OldWoman(Female, Aged):   #Женщина в возрасте
+#     pass
 
 #lena = Girl(shhirt = kletchataya)
+if __name__ == '__main__':
+    lena = Person(sex = 'f', age = 17)
+    f = Female()
+    t = Teen()
+    print(f == lena, lena == t)
